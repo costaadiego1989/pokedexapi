2595b79c1ecb3a632ef41f3ca09ee14fde0aeb11
diego <diego.oliveira@fcamara.com.br>
2022-05-31 23:20:04 -0300

    feat: adicionado popup mostrando os Pokemos salvos no localStorage sem edição de css.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,23 +3,23 @@ import { FaHeart } from "react-icons/fa";
 import { PokemonContext } from "../../context/PokemonContext";
 import React from "react";
 import { useContext } from "react";
+import Swal from "sweetalert2";
 
 export const Navbar = () => {
   const { pokemonList } = useContext(PokemonContext);
-  console.log(pokemonList.length);
-  // const getCountPokemons = JSON.parse(window.localStorage.getItem("pokemons"));
+  console.log(pokemonList);
+  const pokeName = pokemonList.map((pokemon) => {
+    return pokemon.name;
+  });
 
-  // const [pk, setPk] = useState(0);
-
-  // const definePk = (pkm) => {
-  //   setPk(() => pkm);
-  // };
-  // console.log("Log do pkBefore", pk);
-
-  // useEffect(() => {
-  //   if(window.localStorage.hasOwnProperty('pokemons'))
-  //     definePk(getCountPokemons);
-  // }, [pk]);
+  const SwalPopup = () => {
+    Swal.fire({
+      title: `Pokémons Favoritos (${pokemonList.length})`,
+      html: pokeName.toString().split(","),
+      icon: "info",
+      confirmButtonText: "Fechar",
+    });
+  };
 
   return (
     <div>
@@ -31,7 +31,7 @@ export const Navbar = () => {
             </li>
             <li>
               <FaHeart style={{ color: "red", marginRight: "0.5rem" }} />
-              <a href="/">
+              <a href="#" onClick={SwalPopup}>
                 Favoritos ({pokemonList.length})
               </a>
             </li>
